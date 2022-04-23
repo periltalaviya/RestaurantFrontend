e984228bd8285899286b87c9cdd319e48cc7fbec
Peril <>
2022-04-23 11:56:39 +0530

    redirect updated

--- src/components/Reservation/Reservation.js
+++ src/components/Reservation/Reservation.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import axios from 'axios';
 import Navbar from "../Navbar/Navbar";
-import {  useNavigate } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 
 export default function Reservaion() {
 
@@ -35,7 +35,7 @@ export default function Reservaion() {
             data: payload // you are sending body instead
 
         })
-        // console.log(menuName);
+        console.log("c ", payload);
         navigate("/")
 
     }
@@ -58,37 +58,37 @@ export default function Reservaion() {
                                 <p>PLEASE FILL OUT ALL REQUIRED* FIELDS. THANKS!</p>
 
                                 {/* <form id="contact-form" class="reservations-box"> */}
-                                    <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12">
-                                        <div class="form-box">
-                                            <input type="date" onChange={(e) => setBookingDate(e.target.value)} name="dob" id="date" placeholder="Booking Date" required="required" data-error="Booking date is required." />
-                                        </div>
+                                <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12">
+                                    <div class="form-box">
+                                        <input type="date" onChange={(e) => setBookingDate(e.target.value)} name="dob" id="date" placeholder="Booking Date" required="required" data-error="Booking date is required." />
                                     </div>
-                                    <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12">
-                                        <div class="form-box">
-                                            <input type="time" onChange={(e) => setBookingTime(e.target.value)} name="dob" id="date" placeholder="Booking Time" required="required" data-error="Booking time is required." />
-                                        </div>
+                                </div>
+                                <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12">
+                                    <div class="form-box">
+                                        <input type="time" onChange={(e) => setBookingTime(e.target.value)} name="dob" id="date" placeholder="Booking Time" required="required" data-error="Booking time is required." />
                                     </div>
-                                    <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12">
-                                        <div class="form-box">
-                                            <input type="time" onChange={(e) => setApproxArriveTime(e.target.value)} name="dob" id="date" placeholder="Approx Arrival Time" required="required" data-error="Approx Arrival Time is required." />
-                                        </div>
+                                </div>
+                                <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12">
+                                    <div class="form-box">
+                                        <input type="time" onChange={(e) => setApproxArriveTime(e.target.value)} name="dob" id="date" placeholder="Approx Arrival Time" required="required" data-error="Approx Arrival Time is required." />
                                     </div>
-                                    <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12">
-                                        <div class="form-box">
-                                            <input type="number" onChange={(e) => setNoOfPersons(e.target.value)} name="dob" id="date" placeholder="No. of Person" required="required" data-error="No. of Person is required." />
-                                        </div>
+                                </div>
+                                <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12">
+                                    <div class="form-box">
+                                        <input type="number" onChange={(e) => setNoOfPersons(e.target.value)} name="dob" id="date" placeholder="No. of Person" required="required" data-error="No. of Person is required." />
                                     </div>
-                                    <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
-                                        <div class="form-box">
-                                            <textarea onChange={(e) => setComments(e.target.value)} name="comments" placeholder="Comments" />
-                                        </div>
+                                </div>
+                                <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+                                    <div class="form-box">
+                                        <textarea onChange={(e) => setComments(e.target.value)} name="comments" placeholder="Comments" />
                                     </div>
+                                </div>
 
-                                    <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
-                                        <div class="reserve-book-btn text-center">
-                                            <button onClick={postData} class="hvr-underline-from-center" type="submit" value="SEND" id="submit">BOOK MY TABLE </button>
-                                        </div>
+                                <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+                                    <div class="reserve-book-btn text-center">
+                                        <button onClick={postData} class="hvr-underline-from-center" type="submit" value="SEND" id="submit">BOOK MY TABLE </button>
                                     </div>
+                                </div>
                                 {/* </form> */}
 
                             </div>
